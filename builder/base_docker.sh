a423fa21eeb47ffaeafb7875afbe50e5b1ffc6aa
Denis Angell <denisangell7@gmail.com>
2022-09-23 01:55:19 -0400

    refactor

--- builder/base_docker.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-docker build --platform=linux/amd64 --tag transia/builder:1.79.0 . -f debug/Dockerfile && \
-docker create transia/builder:1.79.0 && \
-docker push transia/builder:1.79.0
